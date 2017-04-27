4c4071a26b882c84ef87124455a9f8d564a9746a
Kristjan Broder Lund <kristjan@lund.is>
2017-04-27 19:34:15 +0000

    Do not error if .yardmanrc does not exist

---
+++ spec/cli/read.spec.js
@@ -0,0 +1,19 @@
+const read = require('../../lib/cli/read');
+const { resolve: resolvePath } = require('path');
+
+const subject = filename =>
+  read(resolvePath(__dirname, '../fixtures', filename));
+
+describe('Reading rc file', () => {
+  describe('that exists', () => {
+    it('should return its content', () => {
+      expect(subject('yardmanrc')).to.deep.equal(['--arguments']);
+    });
+  });
+
+  describe('that does not exist', () => {
+    it('should return empty array', () => {
+      expect(subject('not_a_file')).to.deep.equal([]);
+    });
+  });
+});
